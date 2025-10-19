a0dab9108a6bfb6da3cc4f0e547747ff276061f7
Jyotishmoy12 <93263133+Jyotishmoy12@users.noreply.github.com>
2025-10-19 10:37:14 +0530

    okay

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -4,6 +4,7 @@ dotenv.config();
 
 const ServerConfig = {
   PORT: process.env.PORT,
+  REACT_PROJECT_COMMAND: process.env.REACT_PROJECT_COMMAND,
 };
 
 export default ServerConfig;
--- src/controllers/project-controller.js
+++ src/controllers/project-controller.js
@@ -3,18 +3,19 @@ import util from "util"
 import child_process from "child_process";
 import fs from "fs/promises"
 import uuid4 from "uuid4"
-
+import ServerConfig from "../config/server-config.js";
 
 const execPromisified = util.promisify(child_process.exec)
 
 export const createProjectController = async (req, res) => {
   // create a unique id and then inside the projects folder create a new folder with that id
+  const { REACT_PROJECT_COMMAND } = ServerConfig;
   const projectId = uuid4()
   console.log("New project id", projectId)
   await fs.mkdir(`./projects/${projectId}`)
 
   // after this call the npm create vite command in the newly created project folder
-  const response = await execPromisified('npm create vite@latest sandbox -- --template react', {
+  const response = await execPromisified(REACT_PROJECT_COMMAND, {
     cwd: `./projects/${projectId}`
   })
   return res.status(StatusCodes.OK).json({
